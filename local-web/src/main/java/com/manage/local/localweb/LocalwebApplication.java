1c44e2f960b7aa9ef7dadf7dab111ad00482f6ed
xiao liuyang <763119781@qq.com>
2020-06-20 14:58:26 +0800

    create project

---
+++ local-web/src/main/java/com/manage/local/localweb/LocalwebApplication.java
@@ -0,0 +1,13 @@
+package com.manage.local.localweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalwebApplication.class, args);
+    }
+
+}
